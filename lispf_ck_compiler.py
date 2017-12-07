5893916d953f1b9f54d00014c87ef750f7b45604
danielmarques28 <danielmarques7@hotmail.com>
2017-12-07 15:21:53 -0200

    lexicon def defined

--- lispf_ck_compiler.py
+++ lispf_ck_compiler.py
@@ -6,8 +6,8 @@ lexer = ox.make_lexer([
     ('LOOP', r'loop'),
     ('DEC', r'dec'),
     ('INC', r'inc'),
-    ('LPAR', r'\('),
-    ('RPAR', r'\)'),
+    ('LEFT_PAR', r'\('),
+    ('RIGHT_PAR', r'\)'),
     ('RIGHT', r'right'),
     ('LEFT', r'left'),
     ('PRINT', r'print'),
@@ -17,17 +17,18 @@ lexer = ox.make_lexer([
     ('DO_BEFORE', r'do-before'),
     ('ADD', r'add'),
     ('SUB', r'sub'),
-    ('NUMBER', r'[0-9]+'),
-    ('ignore_COMMENT', r';[^\n]*'),
-    ('ignore_BREAK_LINE', r'\n'),
+    ('NUMBER', r'\d+'),
+    ('DEF', r'def'),
+    ('FUCTION', r'(?!loop)(?!dec)(?!inc)(?!right)(?!left)(?!print)(?!read)(?!do)(?!do)(?!do)(?!add)(?!sub)(?!def)(?!\d)[a-zA-Z0-9-\+_]+'),
+    ('ignore_COMMENT', r';[ \S]*'),
     ('ignore_SPACE', r'\s+')
 ])
 
 tokens_list = ['DEC',
           'INC',
           'LOOP',
-          'LPAR',
-          'RPAR',
+          'LEFT_PAR',
+          'RIGHT_PAR',
           'RIGHT',
           'LEFT',
           'PRINT',
@@ -37,27 +38,31 @@ tokens_list = ['DEC',
           'DO_BEFORE',
           'ADD',
           'SUB',
-          'NUMBER']
+          'NUMBER',
+          'DEF',
+          'FUCTION']
 
 parser = ox.make_parser([
-    ('expr : LPAR RPAR', lambda x,y: '()'),
-    ('expr : LPAR term RPAR', lambda x,y,z: y),
+    ('expr : LEFT_PAR RIGHT_PAR', lambda x,y: '()'),
+    ('expr : LEFT_PAR term RIGHT_PAR', lambda x,y,z: y),
     ('term : atom term', lambda x,y: (x,) + y),
-    ('term : atom', lambda x : (x,)),
-    ('atom : expr', lambda x : x),
-    ('atom : DEC', lambda x : x),
-    ('atom : INC', lambda x : x),
-    ('atom : LOOP', lambda x : x),
-    ('atom : RIGHT', lambda x : x),
-    ('atom : LEFT', lambda x : x),
-    ('atom : PRINT', lambda x : x),
-    ('atom : READ', lambda x : x),
-    ('atom : DO', lambda x : x),
-    ('atom : DO_AFTER', lambda x : x),
-    ('atom : DO_BEFORE', lambda x : x),
-    ('atom : ADD', lambda x : x),
-    ('atom : SUB', lambda x : x),
+    ('term : atom', lambda x: (x,)),
+    ('atom : expr', lambda x: x),
+    ('atom : DEC', lambda x: x),
+    ('atom : INC', lambda x: x),
+    ('atom : LOOP', lambda x: x),
+    ('atom : RIGHT', lambda x: x),
+    ('atom : LEFT', lambda x: x),
+    ('atom : PRINT', lambda x: x),
+    ('atom : READ', lambda x: x),
+    ('atom : DO', lambda x: x),
+    ('atom : DO_AFTER', lambda x: x),
+    ('atom : DO_BEFORE', lambda x: x),
+    ('atom : ADD', lambda x: x),
+    ('atom : SUB', lambda x: x),
     ('atom : NUMBER', int),
+    ('atom : DEF', lambda x: x),
+    ('atom : FUCTION', lambda x: x)
 ], tokens_list)
 
 def do_after(command, old_array):
@@ -69,7 +74,6 @@ def do_after(command, old_array):
             i += 1
         new_array.append(old_array[i])
         new_array.append(command)
-
         i += 1
 
     return new_array
@@ -83,15 +87,22 @@ def do_before(command, old_array):
         if old_array[i] == 'add' or old_array[i] == 'sub':
             i += 1
             new_array.append(old_array[i])
-
         i += 1
 
     return new_array
 
+def add_sub(char, number, output_list):
+    i = 0
+    while i < number:
+        output_list.append(char)
+        i += 1
+
+    return output_list
+
+
 def lisp_f_ck_compiler(tree, output_list):
     loop_active = False
     i = 0
-    j = 0
     while i < len(tree):
         if isinstance(tree[i], tuple):
             output_list = lisp_f_ck_compiler(tree[i], output_list)
@@ -105,18 +116,10 @@ def lisp_f_ck_compiler(tree, output_list):
             output_list.append('<')
         elif tree[i] == 'add':
             i += 1
-            while j < tree[i]:
-                output_list.append('+')
-                j += 1
-
-            j = 0
+            output_list = add_sub('+', tree[i], output_list)
         elif tree[i] == 'sub':
             i += 1
-            while j < tree[i]:
-                output_list.append('+')
-                j += 1
-
-            j = 0
+            output_list = add_sub('-', tree[i], output_list)
         elif tree[i] == 'print':
             output_list.append('.')
         elif tree[i] == 'read':
@@ -137,17 +140,19 @@ def lisp_f_ck_compiler(tree, output_list):
             output_list.append('[')
             output_list = lisp_f_ck_compiler(tree[i], output_list)
             output_list.append(']')
-
+        elif tree[i] == 'def':
+            pass
         i += 1
 
     return output_list
 
 def eval(tree):
-    file_code = open('saida.bf', 'w')
+    file_code = open('output.bf', 'w')
     output_list = []
     output_list = lisp_f_ck_compiler(tree, output_list)
     file_code.write(''.join(output_list))
     file_code.close()
+    print('\nOutput file saved with sucess !')
 
 @click.command()
 @click.argument('source', type=click.File('r'))
